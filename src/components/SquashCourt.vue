5d7cd5867a40cfe0bc9a3f662f8220327d97f281
ChrisLoucaides <christosloucaides@gmail.com>
2024-05-28 13:09:27 +0100

    Split court into grid

--- src/components/SquashCourt.vue
+++ src/components/SquashCourt.vue
@@ -3,19 +3,32 @@
     <svg width="480" height="669" viewBox="0 0 160 223" xmlns="http://www.w3.org/2000/svg">
       <rect x="1" y="1" width="158" height="221" fill="none" stroke="red"/>
       <line x1="1" y1="125" x2="159" y2="125" stroke="red"/>
-      <line x1="80" y1="125" x2="80" y2="222" stroke="red" />
-      <line x1="120" y1="125" x2="120" y2="160" stroke="red" />
-      <line x1="40" y1="125" x2="40" y2="160" stroke="red" />
+      <line x1="80" y1="125" x2="80" y2="222" stroke="red"/>
+      <line x1="120" y1="125" x2="120" y2="160" stroke="red"/>
+      <line x1="40" y1="125" x2="40" y2="160" stroke="red"/>
       <line x1="1" y1="160" x2="40.5" y2="160" stroke="red"/>
       <line x1="119.5" y1="160" x2="159" y2="160" stroke="red"/>
+      <!-- Update to a 2x3 grid -->
+      <rect v-for="(color, index) in gridColors" :key="index"
+            :x="1 + (index % 2) * 79" :y="1 + Math.floor(index / 2) * 73.67"
+            width="78" height="73.67" :fill="color"/>
     </svg>
   </div>
 </template>
 
-
 <script>
 export default {
-  name: "SquashCourt"
+  name: 'SquashCourt',
+  data() {
+    return {
+      gridColors: Array(6).fill('transparent')
+    };
+  },
+  methods: {
+    changeColor(index, color) {
+      this.gridColors[index] = color;
+    }
+  }
 }
 </script>
 
