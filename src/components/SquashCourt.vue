e63e5c0f9460bbc553f58e192bebd1b45a3fc0b6
ChrisLoucaides <christosloucaides@gmail.com>
2025-05-18 11:45:57 +0100

    Fix audio issues

--- src/components/SquashCourt.vue
+++ src/components/SquashCourt.vue
@@ -6,21 +6,22 @@
       <h1 v-else-if="cooldownCountdown > 0" class="countdown" style="color: #646cff">{{ cooldownCountdown }}s</h1>
 
       <svg width="480" height="669" viewBox="0 0 160 223" xmlns="http://www.w3.org/2000/svg">
-        <rect x="1" y="1" width="158" height="221" fill="none" stroke="red"/>
-        <line x1="1" y1="125" x2="159" y2="125" stroke="red"/>
-        <line x1="80" y1="125" x2="80" y2="222" stroke="red"/>
-        <line x1="120" y1="125" x2="120" y2="160" stroke="red"/>
-        <line x1="40" y1="125" x2="40" y2="160" stroke="red"/>
-        <line x1="1" y1="160" x2="40.5" y2="160" stroke="red"/>
-        <line x1="119.5" y1="160" x2="159" y2="160" stroke="red"/>
+        <rect x="1" y="1" width="158" height="221" fill="none" stroke="red" />
+        <line x1="1" y1="125" x2="159" y2="125" stroke="red" />
+        <line x1="80" y1="125" x2="80" y2="222" stroke="red" />
+        <line x1="120" y1="125" x2="120" y2="160" stroke="red" />
+        <line x1="40" y1="125" x2="40" y2="160" stroke="red" />
+        <line x1="1" y1="160" x2="40.5" y2="160" stroke="red" />
+        <line x1="119.5" y1="160" x2="159" y2="160" stroke="red" />
         <rect v-for="(color, index) in gridColors" :key="index"
               :x="1 + (index % 2) * 79" :y="1 + Math.floor(index / 2) * 73.67"
-              width="78" height="73.67" :fill="color"/>
+              width="78" height="73.67" :fill="color" />
       </svg>
     </div>
   </div>
 
-  <button class="start" v-if="!intervalId && countdown === 0 && setCountdown === 0 && cooldownCountdown === 0" @click="startColorCycle">
+  <button class="start" v-if="!intervalId && countdown === 0 && setCountdown === 0 && cooldownCountdown === 0"
+          @click="startColorCycle">
     Start Ghosting
   </button>
   <button class="stop" v-else @click="stopColorCycle">
@@ -70,6 +71,7 @@
 export default {
   name: 'SquashCourt',
   data() {
+    const base = import.meta.env.BASE_URL;
     return {
       gridColors: Array(6).fill('transparent'),
       intervalId: null,
@@ -84,12 +86,12 @@ export default {
       durationOptions: [15, 30, 45, 60, 75, 90, 105, 120, 135, 150],
       cooldownOptions: [10, 20, 30, 40, 50, 60],
       sounds: [
-        new Audio('/sounds/front-left.mp3'),
-        new Audio('/sounds/front-right.mp3'),
-        new Audio('/sounds/middle-left.mp3'),
-        new Audio('/sounds/middle-right.mp3'),
-        new Audio('/sounds/back-left.mp3'),
-        new Audio('/sounds/back-right.mp3')
+        new Audio(`${base}/sounds/front-left.mp3`),
+        new Audio(`${base}/sounds/front-right.mp3`),
+        new Audio(`${base}/sounds/middle-left.mp3`),
+        new Audio(`${base}/sounds/middle-right.mp3`),
+        new Audio(`${base}/sounds/back-left.mp3`),
+        new Audio(`${base}/sounds/back-right.mp3`)
       ],
       timeoutIds: [],
       setTimer: null,
